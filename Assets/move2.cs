2d3295cc9bbecd23e026694a51fb0c2976c4ab98
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-10-10 11:06:56 -0500

    cambios nuevo

---
+++ Assets/move2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2 : MonoBehaviour
+{
+private CharacterController controller;
+   private Vector3 playerVelocityInitial;
+   public bool groundedplayer;
+   public float playerSpeed = 20.0f;
+   public float horizontal = 1f;
+
+   public void Start(){
+     controller= gameObject.GetComponent<CharacterController>();
+   }
+    void Update () {
+    //if (GameManager.activado == false) return;
+
+    Vector3 move = new Vector3(horizontal, 0, /*Input.GetAxis("Vertical")*/0);
+    controller.Move(move* Time.deltaTime * playerSpeed);    
+    }
+   public void OnControllerColliderHit(ControllerColliderHit hit){
+    Debug.Log("Choco");
+
+    horizontal = horizontal* -1;
+   }
+}
